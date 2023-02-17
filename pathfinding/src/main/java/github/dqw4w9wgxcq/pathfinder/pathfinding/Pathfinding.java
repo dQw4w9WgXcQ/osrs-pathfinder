d49bfa596f044d240a721e10c06676030d2f3bc0
dQw4w9WgXcQ <22358065+dQw4w9WgXcQ@users.noreply.github.com>
2023-02-17 04:57:37 -0800

    clean up Pathfinding.java

--- pathfinding/src/main/java/github/dqw4w9wgxcq/pathfinder/pathfinding/Pathfinding.java
+++ pathfinding/src/main/java/github/dqw4w9wgxcq/pathfinder/pathfinding/Pathfinding.java
@@ -12,39 +12,15 @@ import github.dqw4w9wgxcq.pathfinder.pathfinding.domain.ComponentGrid;
 import github.dqw4w9wgxcq.pathfinder.pathfinding.linkdistances.LinkDistances;
 import github.dqw4w9wgxcq.pathfinder.pathfinding.localpaths.LocalPaths;
 import github.dqw4w9wgxcq.pathfinder.pathfinding.store.GraphStore;
-import github.dqw4w9wgxcq.pathfinder.pathfinding.store.LinkStore;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.*;
 
 @Slf4j
 @RequiredArgsConstructor
 public class Pathfinding {
-    public static void main(String[] args) throws IOException {
-        var dir = new File(System.getProperty("user.dir"));
-        var linkStore = LinkStore.load(new FileInputStream(new File(dir, "links.zip")));
-        var graphStore = GraphStore.load(new FileInputStream(new File(dir, "graph.zip")), linkStore.links());
-
-        var pathfinding = Pathfinding.create(graphStore);
-
-        var path = pathfinding.findPath(
-                new Position(3433, 3485, 0),
-                new Position(3401, 3486, 0),
-                new Agent(99, Collections.emptyMap(), Collections.emptyList())
-        );
-
-        if (path == null) {
-            System.out.println("no path");
-        } else {
-            System.out.println(path);
-        }
-    }
-
     private final ComponentGrid componentGrid;
     private final ComponentGraph componentGraph;
     private final LinkDistances linkDistances;
@@ -59,8 +35,8 @@ public class Pathfinding {
     }
 
     public PathfindingResult findPath(Position start, Position finish, Agent agent) {
-        start = closestNotBlocked(componentGrid.planes(), start);
-        finish = closestNotBlocked(componentGrid.planes(), finish);
+        start = closestIfBlocked(start);
+        finish = closestIfBlocked(finish);
         if (start == null || finish == null) {
             return new PathfindingResult(start, finish, null);
         }
@@ -72,12 +48,15 @@ public class Pathfinding {
             return new PathfindingResult(start, finish, null);
         }
 
-        var steps = toSteps(linkPath, start, finish);
+        var steps = toPathSteps(linkPath, start, finish);
 
         return new PathfindingResult(start, finish, steps);
     }
 
-    private List<PathStep> toSteps(List<Link> linkPath, Position start, Position finish) {
+    /**
+     * Finds tile paths between links and combines them into a list of path steps.
+     */
+    private List<PathStep> toPathSteps(List<Link> linkPath, Position start, Position finish) {
         var startTime = System.currentTimeMillis();
 
         var curr = start;
@@ -91,8 +70,8 @@ public class Pathfinding {
             curr = link.destination();
         }
 
-        var walkPath = localPaths.get(curr.plane(), curr.point(), finish.point());
-        steps.add(new WalkStep(curr.plane(), walkPath));
+        var tilePath = localPaths.get(curr.plane(), curr.point(), finish.point());
+        steps.add(new WalkStep(curr.plane(), tilePath));
 
         var endTime = System.currentTimeMillis();
         log.info("steps in {}ms", endTime - startTime);
@@ -101,9 +80,8 @@ public class Pathfinding {
     }
 
     /**
-     * Dijkstra-like algorithm.  need to simulate edges to the end point because can't modify the graph.
+     * Dijkstra-like algorithm.  Edges to the end component are simulated to avoid modifying the graph.  (see comment below)
      */
-    //todo pathfinding in reverse (better for teleports, multiple destinations instead of multiple origins)
     private @Nullable List<Link> findLinkPath(Position start, Position finish, Agent agent) {
         record Node(Link link, int distance, boolean isEnd) {
         }
@@ -157,7 +135,7 @@ public class Pathfinding {
                 Collections.reverse(path);
 
                 var endTime = System.currentTimeMillis();
-                log.info("link path in {}ms", endTime - startTime);
+                log.info("link path (took {}ms)", endTime - startTime);
                 return path;
             }
 
@@ -176,24 +154,33 @@ public class Pathfinding {
 
             for (var linkEdge : linkEdges) {
                 var nextLink = linkEdge.link();
-                var nextDistance = curr.distance() + linkEdge.cost();
-                var linkDistance = linkDistances.getOrDefault(nextLink, Integer.MAX_VALUE);
-                if (nextDistance < linkDistance) {
-                    log.debug("adding link {}", nextLink);
-                    queue.add(new Node(nextLink, nextDistance, false));
-                    linkDistances.put(nextLink, nextDistance);
-                    seenFrom.put(nextLink, curr.link());
+
+                if (!agent.hasRequirements(nextLink.requirements())) {
+                    log.debug("requirements not met for link{}", nextLink);
+                    continue;
                 }
+
+                var nextDistance = curr.distance() + linkEdge.cost();
+
+                var linkDistance = linkDistances.getOrDefault(nextLink, Integer.MAX_VALUE);
+                if (nextDistance >= linkDistance) {
+                    continue;
+                }
+
+                log.debug("adding link {}", nextLink);
+                queue.add(new Node(nextLink, nextDistance, false));
+                linkDistances.put(nextLink, nextDistance);
+                seenFrom.put(nextLink, curr.link());
             }
         }
 
         var endTime = System.currentTimeMillis();
-        log.info("no link path in {}ms", endTime - startTime);
+        log.info("no link path (took {}ms)", endTime - startTime);
         return null;
     }
 
-    public static @Nullable Position closestNotBlocked(int[][][] components, Position position) {
-        var plane = components[position.plane()];
+    private @Nullable Position closestIfBlocked(Position position) {
+        var plane = componentGrid.planes()[position.plane()];
         if (plane[position.x()][position.y()] != -1) {
             return position;
         }
@@ -201,9 +188,9 @@ public class Pathfinding {
         var startTime = System.currentTimeMillis();
         log.debug("position {} is blocked, finding closest", position);
 
-        var seen = new BitSet();
-        var frontier = new ArrayDeque<Integer>();
-        frontier.add(position.point().pack());
+        var seen = new HashSet<Point>();
+        var frontier = new ArrayDeque<Point>();
+        frontier.add(position.point());
         var i = 0;
         while (!frontier.isEmpty()) {
             if (i++ > 10000) {
@@ -212,12 +199,10 @@ public class Pathfinding {
             }
 
             var curr = frontier.poll();
-            var currX = Point.unpackX(curr);
-            var currY = Point.unpackY(curr);
-            if (plane[currX][currY] != -1) {
+            if (plane[curr.x()][curr.y()] != -1) {
                 var finishTime = System.currentTimeMillis();
-                log.info("closest position {} in {} from {}", Point.unpack(curr), finishTime - startTime, position);
-                return new Position(currX, currY, position.plane());
+                log.info("closest position {} in {} from {}", curr, finishTime - startTime, position);
+                return new Position(curr.x(), curr.y(), position.plane());
             }
 
             for (var dx = -1; dx <= 1; dx++) {
@@ -231,25 +216,24 @@ public class Pathfinding {
                         continue;
                     }
 
-                    var adjacentX = currX + dx;
-                    var adjacentY = currY + dy;
+                    var adjacentX = curr.x() + dx;
+                    var adjacentY = curr.y() + dy;
 
                     if (adjacentX < 0 || adjacentX >= plane.length || adjacentY < 0 || adjacentY >= plane[0].length) {
                         continue;
                     }
 
-//                    var adjacent = new Point(adjacentX, adjacentY);
-                    var adjacent = Point.pack(adjacentX, adjacentY);
-                    if (seen.get(adjacent)) {
+                    var adjacent = new Point(adjacentX, adjacentY);
+                    if (seen.contains(adjacent)) {
                         continue;
                     }
 
-                    seen.set(adjacent);
+                    seen.add(adjacent);
                     frontier.add(adjacent);
                 }
             }
         }
 
-        throw new IllegalStateException("No position found");
+        throw new IllegalStateException("No position found");//will never happen unless the graph is invalid
     }
 }
