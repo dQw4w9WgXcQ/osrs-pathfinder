e3f650da3117697b13f52739cc28f170b0368e65
dQw4w9WgXcQ <22358065+dQw4w9WgXcQ@users.noreply.github.com>
2023-12-16 00:13:34 -0800

    change GridStore.java from java object serialization to agnostic binary format that can be read in Rust

--- commons/src/main/java/dev/dqw4w9wgxcq/pathfinder/commons/store/GridStore.java
+++ commons/src/main/java/dev/dqw4w9wgxcq/pathfinder/commons/store/GridStore.java
@@ -1,16 +1,16 @@
 package dev.dqw4w9wgxcq.pathfinder.commons.store;
 
 import com.google.gson.Gson;
-import lombok.SneakyThrows;
+import dev.dqw4w9wgxcq.pathfinder.commons.Constants;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
@@ -35,19 +35,21 @@ public record GridStore(byte[][][] grid) {
             zos.write(GSON.toJson(new Meta(grid[0].length, grid[0][0].length)).getBytes());
 
             zos.putNextEntry(new ZipEntry("grid.dat"));
-            try (var oos = new ObjectOutputStream(zos)) {
-                log.info("writing grid.dat");
-                oos.writeObject(grid);
+            try (var dos = new DataOutputStream(zos)) {
+                for (var plane : grid) {
+                    for (var col : plane) {
+                        dos.write(col);
+                    }
+                }
             }
         }
     }
 
-    @SneakyThrows(ClassNotFoundException.class)
-    public static GridStore load(InputStream is) throws IOException {
-        log.info("loading grid from {}", is);
+    public static GridStore load(File file) throws IOException {
+        log.info("loading grid from {}", file);
 
         byte[][][] grid;
-        try (is;
+        try (var is = new FileInputStream(file);
              var zis = new ZipInputStream(is)) {
             log.info("reading meta.json");
             zis.getNextEntry();
@@ -56,11 +58,38 @@ public record GridStore(byte[][][] grid) {
 
             log.info("reading grid.dat");
             zis.getNextEntry();
-            try (var ois = new ObjectInputStream(zis)) {
-                grid = (byte[][][]) ois.readObject();
+            try (var dis = new DataInputStream(zis)) {
+                grid = new byte[Constants.PLANES_SIZE][meta.width][meta.height];
+                for (var plane = 0; plane < Constants.PLANES_SIZE; plane++) {
+                    for (var x = 0; x < meta.width; x++) {
+                        dis.readFully(grid[plane][x]);
+                    }
+                }
             }
         }
 
         return new GridStore(grid);
+    }
+
+    public static void main(String[] args) throws IOException {
+        var dir = new File(System.getProperty("user.dir"), "asdf");
+
+        var grid = new byte[Constants.PLANES_SIZE][10][15];
+
+        grid[0][0][0] = 1;
+        grid[0][0][1] = 3;
+        grid[0][0][2] = 5;
+        grid[1][3][3] = 7;
+        grid[0][3][4] = (byte) (1 << 7);
+
+        new GridStore(grid).save(dir);
+
+        var loaded = GridStore.load(new File(dir, "grid.zip"));
+
+        System.out.println(loaded.grid[0][0][0]);
+        System.out.println(loaded.grid[0][0][1]);
+        System.out.println(loaded.grid[0][0][2]);
+        System.out.println(loaded.grid[1][3][3]);
+        System.out.println(loaded.grid[0][3][4]);
     }
 }
