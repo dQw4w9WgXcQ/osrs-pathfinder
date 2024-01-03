9c7efdcac3f6b69e1b8b27d4dda9ae8abd0aaf83
dQw4w9WgXcQ <22358065+dQw4w9WgXcQ@users.noreply.github.com>
2024-01-02 20:06:57 -0800

    change GridStore format

--- commons/src/main/java/dev/dqw4w9wgxcq/pathfinder/commons/store/GridStore.java
+++ commons/src/main/java/dev/dqw4w9wgxcq/pathfinder/commons/store/GridStore.java
@@ -10,7 +10,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
@@ -30,13 +30,12 @@ public record GridStore(byte[][][] grid) {
 
         try (var fos = new FileOutputStream(new File(dir, "grid.zip"));
              var zos = new ZipOutputStream(fos)) {
-            log.debug("writing meta.json");
-            zos.putNextEntry(new ZipEntry("meta.json"));
-            zos.write(GSON.toJson(new Meta(grid[0].length, grid[0][0].length)).getBytes());
 
             log.debug("writing grid.dat");
             zos.putNextEntry(new ZipEntry("grid.dat"));
             try (var dos = new DataOutputStream(zos)) {
+                dos.writeInt(grid[0].length);
+                dos.writeInt(grid[0][0].length);
                 for (var plane : grid) {
                     for (var col : plane) {
                         dos.write(col);
@@ -52,17 +51,15 @@ public record GridStore(byte[][][] grid) {
         byte[][][] grid;
         try (var is = new FileInputStream(file);
              var zis = new ZipInputStream(is)) {
-            log.debug("reading meta.json");
-            zis.getNextEntry();
-            var meta = GSON.fromJson(new InputStreamReader(zis), Meta.class);
-            log.debug("meta: {}", meta);
-
             log.debug("reading grid.dat");
             zis.getNextEntry();
             try (var dis = new DataInputStream(zis)) {
-                grid = new byte[Constants.PLANES_SIZE][meta.width][meta.height];
+                var width = dis.readInt();
+                var height = dis.readInt();
+
+                grid = new byte[Constants.PLANES_SIZE][width][height];
                 for (var plane = 0; plane < Constants.PLANES_SIZE; plane++) {
-                    for (var x = 0; x < meta.width; x++) {
+                    for (var x = 0; x < width; x++) {
                         dis.readFully(grid[plane][x]);
                     }
                 }
@@ -73,7 +70,7 @@ public record GridStore(byte[][][] grid) {
     }
 
     public static void main(String[] args) throws IOException {
-        var dir = new File(System.getProperty("user.dir"), "asdf");
+        var dir = new File(new File(System.getProperty("user.home"), "Desktop"), "asdf");
 
         var grid = new byte[Constants.PLANES_SIZE][10][15];
 
@@ -82,6 +79,7 @@ public record GridStore(byte[][][] grid) {
         grid[0][0][2] = 5;
         grid[1][3][3] = 7;
         grid[0][3][4] = (byte) (1 << 7);
+        grid[0][9][14] = 11;
 
         new GridStore(grid).save(dir);
 
@@ -92,5 +90,8 @@ public record GridStore(byte[][][] grid) {
         System.out.println(loaded.grid[0][0][2]);
         System.out.println(loaded.grid[1][3][3]);
         System.out.println(loaded.grid[0][3][4]);
+        System.out.println(loaded.grid[0][9][14]);
+
+        System.out.println(Arrays.deepToString(loaded.grid()));
     }
 }
